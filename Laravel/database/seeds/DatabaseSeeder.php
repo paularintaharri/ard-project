610632ca820f535cec46eb1b3a1c3b548a929326
Timo Lehtola <timo.lehtola@metropolia.fi>
2017-10-04 21:08:36 +0300

    Database fakers done for users and tickets

--- Laravel/database/seeds/DatabaseSeeder.php
+++ Laravel/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        factory(App\User::class, 10)->create();
+        factory(App\Ticket::class, 20)->create();
         // $this->call(UsersTableSeeder::class);
     }
 }
